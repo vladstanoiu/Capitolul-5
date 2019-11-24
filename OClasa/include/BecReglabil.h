ebbbc9393e24ab17bfb0b2bb7d7441b2e10bc8f1
vladstanoiu <55551799+vladstanoiu@users.noreply.github.com>
2019-11-24 18:00:57 +0200

    Create BecReglabil.h

---
+++ OClasa/include/BecReglabil.h
@@ -0,0 +1,35 @@
+#ifndef BECREGLABIL_H
+#define BECREGLABIL_H
+#include <iostream>
+
+class BecReglabil
+{
+private:
+    unsigned short putereMaxima;
+    unsigned short putereCurenta;
+    bool aprins;
+
+    public:
+
+        void SetPutereMaxima (unsigned short putMaxima) { putereMaxima = putMaxima;}
+        void SetPutereCurenta (unsigned short putCurenta) {putereCurenta = putCurenta;}
+
+        unsigned short GetPutereMaxima () {return putereMaxima;}
+        unsigned short GetPutereCurenta () {return putereCurenta;}
+
+        void Aprinde();
+        void Stinge();
+        short MaresteLumina(short);
+        short ReduceLumina (short);
+        void StareaBec();
+
+
+        BecReglabil();
+        virtual ~BecReglabil();
+
+    protected:
+
+
+};
+
+#endif // BECREGLABIL_H
